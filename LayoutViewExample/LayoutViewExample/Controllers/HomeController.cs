bf0a1357e6cb7cc7230040632d7e0be3565e9bd1
Zahir Shaikh <152019891+Zahhiiir@users.noreply.github.com>
2025-10-25 18:52:35 +0530

    Add files via upload
    Layout views project practice project

---
+++ LayoutViewExample/LayoutViewExample/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LayoutViewExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        [Route("/home")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("/about-company")]
+        public IActionResult About()
+        {
+            return View();
+        }
+        [Route("/contact-support")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ LayoutViewExample/LayoutViewExample/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LayoutViewExample.Controllers
+{
+    public class ProductsController : Controller
+    {
+        [Route("/products")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //product id is optional
+        [Route("/products/search/{ProductID}")]
+        [Route("/products/search")]
+
+        public IActionResult Search(int? ProductID)
+        {
+            ViewBag.ProductID = ProductID;
+            return View();
+        }
+        [Route("/products/order")]
+        public IActionResult Order()
+        {
+            return View();
+        }
+
+    }
+}
