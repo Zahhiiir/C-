828fe9f6660c721dbad51de9bc5c3a2ced13b72e
Zahir Shaikh <152019891+Zahhiiir@users.noreply.github.com>
2025-10-31 17:36:00 +0530

    Add files via upload
    Partial view example project

---
+++ PartialViewsExample/PartialViewsExample/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartialViewsExample.Models;
+
+namespace PartialViewsExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        [Route("/home")]
+        public IActionResult Index()
+        {
+            ListModel model = new ListModel();
+            return View(model);
+        }
+        [Route("/about")]
+        public IActionResult About()
+        {
+            return View();
+        }
+        [Route("/programming-languages")]
+        public IActionResult ProgrammingLanguages()
+        {
+            // Simplified collection initialization
+            ListModel listmodel = new ListModel
+            {
+                ListTitle = "Programming Languages",
+                ListItems = new List<string>
+                    {
+                        "C#",
+                        "Java",
+                        "Python",
+                        "JavaScript",
+                        "Go",
+                        "Ruby"
+                    }
+            };
+
+            return PartialView("_ListPartialView",listmodel); // Pass the model to the view
+        }
+    }
+}
---
+++ PartialViewsExample/PartialViewsExample/Models/ListModel.cs
@@ -0,0 +1,8 @@
+﻿namespace PartialViewsExample.Models
+{
+    public class ListModel
+    {
+        public String ListTitle { get; set; }= "My List";
+        public List<String> ListItems { get; set; } = new List<String>();
+    }
+}
