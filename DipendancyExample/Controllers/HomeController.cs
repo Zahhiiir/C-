6b98fd9afdd1a7419c5d2aae2eb9d8a8d3922796
Zahir Shaikh <152019891+Zahhiiir@users.noreply.github.com>
2025-11-19 18:20:07 +0530

    Add files via upload
    dipendancy

---
+++ DipendancyExample/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services;
+using ServiceContract;
+
+namespace DipendancyExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ICitiesService _citiesService;
+
+        public HomeController(ICitiesService citiesService)
+        {
+            _citiesService = citiesService;  //new ICitiesService();
+        }
+        [Route("/")]
+        public IActionResult Index()
+        {
+           List<string>? Cities = _citiesService.GetCities();
+            return View(Cities);
+        }
+    }
+}
